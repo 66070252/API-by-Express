62a7cecb4cd25b7b713f55b5097272c79ac70411
66070252 <66070252@kmitl.ac.th>
2025-07-28 00:46:41 +0700

    Update day3-api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { connect } = require('http2');
 const mysql = require('mysql2');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,7 +24,7 @@ db.connect((err) => {
 });
 
 app.get('/products', (req, res) => {
-    db.query('SELECT * FROM products', (err, results) => {
+    db.query('SELECT * FROM products WHERE is_deleted = 0', (err, results) => {
         if (err) {
             console.error('MySQL Error:', err);
             return res.status(500).json({ error: 'ไม่สามารถดึงข้อมูลสินค้า' });
@@ -35,7 +36,7 @@ app.get('/products', (req, res) => {
 app.get('/products/:id', (req, res) => {
     const productId = req.params.id;
     
-    const sql = 'SELECT * FROM products WHERE id = ?';
+    const sql = 'SELECT * FROM products WHERE id = ? AND is_deleted = 0';
     db.query(sql, [productId], (err, results) => {
         if (err) {
             return res.status(500).json({ error: 'เกิดข้อผิดพลาด' });
@@ -51,7 +52,7 @@ app.get('/products/:id', (req, res) => {
 
 app.get('/products/search/:keyword', (req, res) => {
     const keyword = req.params.keyword;
-    const sql = 'SELECT * FROM products WHERE name LIKE ?';
+    const sql = 'SELECT * FROM products WHERE name LIKE ? AND is_deleted = 0';
     
     db.query(sql, [`%${keyword}%`], (err, results) => {
         if (err) {
@@ -63,6 +64,52 @@ app.get('/products/search/:keyword', (req, res) => {
         }
         res.json(results);
     });
+});
+
+app.post('/products', (req, res) => {
+    const { name, price, discount, review_count, image_url } = req.body;
+    db.query(
+        'INSERT INTO products (name, price, discount, review_count, image_url) VALUES (?, ?, ?, ?, ?)',
+        [name, price, discount, review_count, image_url],
+        (err, result) => {
+            if (err) return res.status(500).json({ error: err.message });
+            res.status(201).json({ id: result.insertId, message: 'Product created'});
+        }
+    );
+});
+
+app.put('/products/:id', (req, res) => {
+  const { name, price, discount, review_count, image_url } = req.body;
+  db.query(
+    'UPDATE products SET name = ?, price = ?, discount = ?, review_count = ?, image_url = ? WHERE id = ?',
+    [name, price, discount, review_count, image_url, req.params.id],
+    (err) => {
+            if (err) return res.status(500).json({error: err.message});
+            res.json({ message: 'Product updated'});
+        }
+    );
+});
+
+app.delete('/products/:id', (req, res) => {
+    db.query(
+        'UPDATE products SET is_deleted = 1 WHERE id = ?',
+        [req.params.id],
+        (err) => {
+            if (err) return res.status(500).json({ error: err.message});
+            res.json({ message: 'Product soft-deleted' });
+        }
+    );
+});
+
+app.put('/products/restore/:id', (req, res) => {
+    db.query(
+        'UPDATE products SET is_deleted = 0 WHERE id=?',
+        [req.params.id],
+        (err) => {
+            if (err) return res.status(500).json({ error: err.message});
+            res.json({ message: 'Product restored'});
+        }
+    )
 });
 
 app.listen(port, () => {
